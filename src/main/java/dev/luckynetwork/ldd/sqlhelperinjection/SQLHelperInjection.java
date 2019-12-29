096d1534401562000e4373e70fc22490c14e800b
Alviann <alviandq@yahoo.com>
2019-12-29 09:41:17 +0700

    Initial commit

---
+++ src/main/java/dev/luckynetwork/ldd/sqlhelperinjection/SQLHelperInjection.java
@@ -0,0 +1,33 @@
+package dev.luckynetwork.ldd.sqlhelperinjection;
+
+import com.github.alviannn.lib.dependencyhelper.DependencyHelper;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SQLHelperInjection extends JavaPlugin {
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    @Override
+    public void onEnable() {
+        DependencyHelper helper = new DependencyHelper(this.getClass().getClassLoader());
+        File dir = new File(this.getDataFolder(), "libs");
+
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+
+        try {
+            helper.download("", "https://github.com/Alviannn/SQLHelper/releases/download/2.4/SQLHelper-2.4.jar", dir.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
