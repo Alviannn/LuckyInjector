4b678899c99b335269e71814c61b9cf9c186d4d3
Alviann <alviandq@yahoo.com>
2020-01-01 17:50:59 +0700

    * Fixed SQLHelper not loading * Another attempt to fix SQLHelper download

--- src/main/java/dev/luckynetwork/ldd/sqlhelperinjection/SQLHelperInjection.java
+++ src/main/java/dev/luckynetwork/ldd/sqlhelperinjection/SQLHelperInjection.java
@@ -4,7 +4,6 @@ import com.github.alviannn.lib.dependencyhelper.DependencyHelper;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.IOException;
 
 public class SQLHelperInjection extends JavaPlugin {
 
@@ -14,13 +13,17 @@ public class SQLHelperInjection extends JavaPlugin {
         DependencyHelper helper = new DependencyHelper(this.getClass().getClassLoader());
         File dir = new File(this.getDataFolder(), "libs");
 
+        if (!this.getDataFolder().exists()) {
+            this.getDataFolder().mkdir();
+        }
         if (!dir.exists()) {
             dir.mkdir();
         }
 
         try {
             helper.download("SQLHelper-2.4.jar", "https://github.com/Alviannn/SQLHelper/releases/download/2.4/SQLHelper-2.4.jar", dir.toPath());
-        } catch (IOException e) {
+            helper.loadDir(dir.toPath());
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
