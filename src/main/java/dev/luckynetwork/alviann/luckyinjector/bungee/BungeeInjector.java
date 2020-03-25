18bf587959dc994de99eed5cde3c2230ec12cf4d
Alviannn <alviandq@yahoo.com>
2020-03-25 09:21:25 +0700

    1.5.1 update:
    * Removed getPlugin method on both injector class
    * Removed useless instance initiation and check on loadEarly method
    * Should fix most issues

--- src/main/java/dev/luckynetwork/alviann/luckyinjector/bungee/BungeeInjector.java
+++ src/main/java/dev/luckynetwork/alviann/luckyinjector/bungee/BungeeInjector.java
@@ -2,20 +2,15 @@ package dev.luckynetwork.alviann.luckyinjector.bungee;
 
 import com.github.alviannn.sqlhelper.SQLBuilder;
 import com.github.alviannn.sqlhelper.SQLHelper;
-import com.github.alviannn.sqlhelper.utils.Closer;
 import dev.luckynetwork.alviann.luckyinjector.loader.Loader;
 import lombok.Getter;
 import lombok.SneakyThrows;
-import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.api.plugin.PluginManager;
 import net.md_5.bungee.config.Configuration;
 import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 
 import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class BungeeInjector extends Plugin {
 
@@ -25,47 +20,15 @@ public class BungeeInjector extends Plugin {
     @Override
     public void onEnable() {
         instance = this;
-
-        Loader.startInjecting(this.getClass());
-        Loader.initConfig(this.getClass());
+        BungeeInjector.loadEarly();
     }
 
     /**
      * loads the injector earlier
      */
     public static void loadEarly() throws Exception {
-        Plugin plugin = getPlugin();
-
-        if (instance == null && plugin != null)
-            instance = (BungeeInjector) plugin;
-        if (instance == null)
-            instance = new BungeeInjector();
-
-        Loader.startInjecting(instance.getClass());
-        Loader.initConfig(instance.getClass());
-    }
-
-    private static Plugin getPlugin() {
-        PluginManager manager = ProxyServer.getInstance().getPluginManager();
-        String pluginName;
-
-        try (Closer closer = new Closer()) {
-            ClassLoader loader = BungeeInjector.class.getClassLoader();
-            InputStream stream = loader.getResourceAsStream("bungee.yml");
-
-            if (stream == null)
-                return null;
-
-            closer.add(stream);
-
-            InputStreamReader streamReader = closer.add(new InputStreamReader(stream));
-            ConfigurationProvider provider = ConfigurationProvider.getProvider(YamlConfiguration.class);
-            Configuration plugin = provider.load(streamReader);
-
-            pluginName = plugin.getString("name");
-        }
-
-        return manager.getPlugin(pluginName);
+        Loader.startInjecting(BungeeInjector.class);
+        Loader.initConfig(BungeeInjector.class);
     }
 
     /**
@@ -80,7 +43,7 @@ public class BungeeInjector extends Plugin {
      */
     @SneakyThrows
     public static SQLBuilder getDefaultSQLBuilder() {
-        Loader.initConfig(instance.getClass());
+        Loader.initConfig(BungeeInjector.class);
 
         File configFile = Loader.CONFIG_FILE;
         ConfigurationProvider provider = ConfigurationProvider.getProvider(YamlConfiguration.class);
--- src/main/java/dev/luckynetwork/alviann/luckyinjector/spigot/SpigotInjector.java
+++ src/main/java/dev/luckynetwork/alviann/luckyinjector/spigot/SpigotInjector.java
@@ -2,21 +2,14 @@ package dev.luckynetwork.alviann.luckyinjector.spigot;
 
 import com.github.alviannn.sqlhelper.SQLBuilder;
 import com.github.alviannn.sqlhelper.SQLHelper;
-import com.github.alviannn.sqlhelper.utils.Closer;
-import dev.luckynetwork.alviann.luckyinjector.bungee.BungeeInjector;
 import dev.luckynetwork.alviann.luckyinjector.loader.Loader;
 import lombok.Getter;
 import lombok.SneakyThrows;
-import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class SpigotInjector extends JavaPlugin {
 
@@ -26,46 +19,15 @@ public class SpigotInjector extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-
-        Loader.startInjecting(this.getClass());
-        Loader.initConfig(this.getClass());
+        SpigotInjector.loadEarly();
     }
 
     /**
      * loads the injector earlier
      */
     public static void loadEarly() throws Exception {
-        Plugin plugin = getPlugin();
-
-        if (instance == null && plugin != null)
-            instance = (SpigotInjector) plugin;
-        if (instance == null)
-            instance = new SpigotInjector();
-
-        Loader.startInjecting(instance.getClass());
-        Loader.initConfig(instance.getClass());
-    }
-
-    private static Plugin getPlugin() {
-        PluginManager manager = Bukkit.getPluginManager();
-        String pluginName;
-
-        try (Closer closer = new Closer()) {
-            ClassLoader loader = BungeeInjector.class.getClassLoader();
-            InputStream stream = loader.getResourceAsStream("bungee.yml");
-
-            if (stream == null)
-                return null;
-
-            closer.add(stream);
-
-            InputStreamReader streamReader = closer.add(new InputStreamReader(stream));
-            FileConfiguration plugin = YamlConfiguration.loadConfiguration(streamReader);
-
-            pluginName = plugin.getString("name");
-        }
-
-        return manager.getPlugin(pluginName);
+        Loader.startInjecting(SpigotInjector.class);
+        Loader.initConfig(SpigotInjector.class);
     }
 
     /**
@@ -80,7 +42,7 @@ public class SpigotInjector extends JavaPlugin {
      */
     @SneakyThrows
     public static SQLBuilder getDefaultSQLBuilder() {
-        Loader.initConfig(instance.getClass());
+        Loader.initConfig(SpigotInjector.class);
 
         File configFile = Loader.CONFIG_FILE;
         FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
